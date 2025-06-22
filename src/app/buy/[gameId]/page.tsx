4c6a7bcb11d1ba02a0e4c49fb94103f0d44b8ca9
Najmul Huda <najmul0834@gmail.com>
2025-06-22 08:29:35 +0000

    still not working

--- src/app/buy/[gameId]/page.tsx
+++ src/app/buy/[gameId]/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useState, useRef } from "react";
+import { useState, useRef, use } from "react";
 import { notFound, useParams } from "next/navigation"
 import Image from "next/image"
 import { CalendarDays, MapPin, Ticket, CreditCard, User as UserIcon, CheckCircle, Download } from "lucide-react"
